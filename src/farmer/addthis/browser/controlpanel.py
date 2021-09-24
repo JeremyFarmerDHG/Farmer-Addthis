716402757b740d969ac317ca1cb77d5e52f1adb1
Jeremy Farmer <Jeremy.Farmer@dhg.com>
2021-09-24 10:52:59 -0400

    added back url and share options

--- src/farmer/addthis/browser/controlpanel.py
+++ src/farmer/addthis/browser/controlpanel.py
@@ -12,30 +12,30 @@ class IAddThisControlPanel(Interface):
         default=False,
     )
 
-    # show_on_urls = schema.Tuple(
-    #     title = u'Show On Urls',
-    #     default=(),
-    #     missing_value=None,
-    #     required=False,
-    #     value_type=schema.TextLine()
-    # )
+    show_on_urls = schema.Tuple(
+        title = u'Show On Urls',
+        default=(),
+        missing_value=None,
+        required=False,
+        value_type=schema.TextLine()
+    )
 
-    # blacklist_urls = schema.Tuple(
-    #     title = u'Blacklist Urls',
-    #     default=(),
-    #     missing_value=None,
-    #     required=False,
-    #     value_type=schema.TextLine()
-    # )
+    blacklist_urls = schema.Tuple(
+        title = u'Blacklist Urls',
+        default=(),
+        missing_value=None,
+        required=False,
+        value_type=schema.TextLine()
+    )
 
-    # # make this a choice on a vocab
-    # share_options = schema.Tuple(
-    #     title = u'Share Options',
-    #     default=(),
-    #     missing_value=None,
-    #     required=False,
-    #     value_type=schema.TextLine()
-    # )
+    # make this a choice on a vocab
+    share_options = schema.Tuple(
+        title = u'Share Options',
+        default=(),
+        missing_value=None,
+        required=False,
+        value_type=schema.TextLine()
+    )
 
 class AddThisControlPanelForm(RegistryEditForm):
     schema = IAddThisControlPanel
